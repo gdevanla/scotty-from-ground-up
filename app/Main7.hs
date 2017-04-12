f68defa77a3a4e8a57042905f7f5eb4a5d3d6a88
gdevanla <gdrvnl@gmail.com>
2017-04-12 08:18:55 -0700

    clean-up main7, add Main8 with notes

--- app/Main7.hs
+++ app/Main7.hs
@@ -1,15 +1,13 @@
-{-# LANGUAGE FlexibleContexts #-}
 --- Demonstrate handling routes only if previous one
-
+import Data.List
 import qualified Control.Monad.Trans.State.Strict as ST
 import qualified Control.Monad.Trans.Except as Exc
 import Control.Monad.Reader
 import Control.Monad.Error.Class
 
--- State Monad
--- How to use a State Monad
-
+--
 type Response = String
+type Request = String
 
 type ActionT a = Exc.ExceptT ActionError (ReaderT String (ST.State Response)) a
 
@@ -22,74 +20,77 @@ type ActionError = String
 
 type AppStateT = ST.State AppState
 
---type ActionT = Exc.ExceptT ActionError Maybe String
 
 add_middleware' mf s@(AppState {middlewares = mw}) = s {middlewares = mf:mw}
 
-middleware_func1 :: ActionT ()
-middleware_func1 = do
+construct_response args = intercalate "\n" args
+
+route_handler1 :: ActionT ()
+route_handler1 = do
   input_string <- ask
-  lift . lift $ (ST.modify $ (\s -> s ++ input_string ++ "inside middleware func"))
+  let st = (
+        ST.modify
+          $ (\s -> construct_response [s, input_string, ": from route_handler1"]))
+  lift . lift $ st
 
-  --ST.modify (\s -> s ++  "..and..input= " ++ input)
-  --return $ ""
+route_handler2 :: ActionT ()
+route_handler2 = do
+  input_string <- ask
+  lift . lift $ (ST.modify $ (\s -> s ++ input_string ++ " inside middleware func 2"))
 
--- middleware_func2 :: ActionT String
--- middleware_func2 = do
---   input <- ask
---   return $ "middlware_func2 got input = " ++ input
 
-middleware_func3_buggy :: ActionT ()
-middleware_func3_buggy = throwError "error from buggy handler"
+route_handler3_buggy :: ActionT ()
+route_handler3_buggy = throwError "error from buggy handler"
 
--- middleware_func3 :: ActionT String
--- middleware_func3 = do
---   input <- ask
---   return $ "middlware_func3 got_input = " ++ input
-
-handler :: ActionT String
-handler = do
-  input <- ask
-  return $ "handler with input = " ++ input
+route_handler3 :: ActionT ()
+route_handler3 = do
+  input_string <- ask
+  lift . lift $ (ST.modify $ (\s -> s ++ input_string ++ " inside middleware func 3"))
 
 add_middleware mf pat = ST.modify $ \s -> add_middleware' (route mf pat) s
 
 cond condition_str = f where
   f i = i == condition_str
 
-route :: ActionT () -> (String -> Bool) -> Route
-route mw pat mw1 input_string =
-  let tryNext = mw1 input_string in
-  if pat input_string
+route :: ActionT () -> (Request -> Bool) -> Route
+route mw pat mw1 request =
+  let tryNext = mw1 request in
+  if pat request
   then
-    let x = runAction mw input_string
+    let x = runAction mw request
     in
       case x of
         Just x2 -> x2
-        Nothing -> "Nothing..."
+        Nothing -> ""
   else
     tryNext
 
-runAction :: ActionT () -> String -> Maybe Response
-runAction action input_string =
-  let (a, s) = (flip ST.runState "initial_state")
-        $ (flip runReaderT input_string) $ (Exc.runExceptT action)
-  in
-  either (const $ Just $ "error:" ++ s) (const (Just $ s ++ "..state updated in run_action..")) a
+runAction :: ActionT () -> Request -> Maybe Response
+runAction action request =
+  let (a, s) = (flip ST.runState "")
+               $ (flip runReaderT request)
+               $ (Exc.runExceptT action)
+      left = (const $ Just $ "There was an error")
+      right = (const (Just $ s ++ "..state updated in run_action..")) in
+    either  left right a
 
 myApp :: AppStateT ()
 myApp = do
-  add_middleware middleware_func1 (\s -> s == "middleware11")
-  add_middleware middleware_func3_buggy (\s -> s == "middleware1")
-  -- add_middleware middleware_func2 (\s -> s == "middleware2")
-  -- add_middleware middleware_func3 (\s -> s == "middleware3")
+  add_middleware route_handler1 (\s -> s == "middleware11")
+  add_middleware route_handler3_buggy (\s -> s == "middleware1")
+  add_middleware route_handler2 (\s -> s == "middleware2")
+  add_middleware route_handler3 (\s -> s == "middleware3")
 
 runMyApp initial_string my_app =
   let s = ST.execState my_app $ AppState { middlewares = []}
       output = foldl (flip ($)) initial_string (middlewares s) in
   output
 
+defRoute _ = "There was no route defined to process your request."
+
 main = do
-  print $ "Starting demonstration of middlewares"
-  let x1 = runMyApp (\x-> "default middlware called") myApp "middleware11" in
-    print $ show $ x1
+  putStrLn "Please type in the request."
+  request <- getLine
+  putStrLn $ "Starting demonstration of middlewares"
+  let x1 = runMyApp defRoute myApp request in
+    putStrLn $ x1
