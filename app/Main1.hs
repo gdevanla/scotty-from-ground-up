955328d9b44e092a321e6e99747cbc9520d11039
gdevanla <gdrvnl@gmail.com>
2017-04-18 17:57:30 -0700

    update main1 and main3 along with the introduction section

--- app/Main1.hs
+++ app/Main1.hs
@@ -4,34 +4,29 @@ import qualified Control.Monad.Trans.State.Strict as ST
 import Data.List
 import Control.Monad
 
--- State Monad
--- How to use a State Monad
-
 type Route = String -> String
 
 data AppState = AppState { routes::[Route]}
-
 type AppStateT = ST.State AppState
 
+
+-- client functions
 constructResponse :: [String] -> String
 constructResponse = unwords
 
-addRoute' :: Route -> AppState -> AppState
-addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
 
-routeHandler1 :: [Char] -> String
+routeHandler1 :: String -> String
 routeHandler1 request =
   constructResponse [
   "\nrequest in handler1: got " ++ request]
 
-
+routeHandler2 :: String -> String
 routeHandler2 request = constructResponse [
       "\n\trequest in handler2 got :" ++ request]
 
+routeHandler3 :: String -> String
 routeHandler3 request = constructResponse [
   "\n\t\trequest in handler3:" ++ request]
-
-addRoute mf = ST.modify $ \s -> addRoute' mf s
 
 myApp :: AppStateT ()
 myApp = do
@@ -39,18 +34,34 @@ myApp = do
   addRoute routeHandler2
   addRoute routeHandler3
 
+main :: IO ()
+main = myScotty myApp
+
+--  framework methods
+
+addRoute' :: Route -> AppState -> AppState
+addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
+
+addRoute mf = ST.modify $ \s -> addRoute' mf s
+
+runMyApp :: Monad m => String -> AppState -> m String
 runMyApp initial_string my_app = do
-  let s = ST.execState my_app AppState{ routes = []}
-  let output = foldl (flip ($)) initial_string (routes s)
+  let output = foldl (flip ($)) initial_string (routes my_app)
   return $ output
 
-main = do
+userInputLoop app_state = do
   putStrLn "Please type in the request"
   putStrLn "(one of 'handler1', 'handler2', 'handler3', 'buggy' or any string for default handling)"
   request <- getLine
+
   unless (request == "q") $ do
-    let response = runMyApp request myApp
+    let response = runMyApp request app_state
     case response of
       Just x -> putStrLn x
       Nothing -> putStrLn "Error"
-    main
+    userInputLoop app_state
+
+myScotty my_app = do
+  let app_state = ST.execState my_app AppState{routes=[]}
+  userInputLoop app_state
+  
--- app/Main3.hs
+++ app/Main3.hs
@@ -4,34 +4,51 @@ import qualified Control.Monad.Trans.State.Strict as ST
 import Data.List
 import Control.Monad
 
--- State Monad
--- How to use a State Monad
 
 type Application = String -> String
 type Route = Application -> Application
 
 data AppState = AppState { routes:: [Route]}
-
 type AppStateT = ST.State AppState
 
-addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
+--client functions
 
+constructResponse :: [String] -> String
 constructResponse = unwords
 
+routeHandler1 :: String -> String
 routeHandler1 request =
   constructResponse [
   "request in handler1: got " ++ request]
 
+routeHandler2 :: String -> String
 routeHandler2 request = constructResponse [
       "request in handler2 got :" ++ request]
 
+routeHandler3 :: String -> String
 routeHandler3 request = constructResponse [
   "request in handler3:" ++ request]
 
+defaultRoute :: String -> String
 defaultRoute request = constructResponse [
   request , "processed by defaultRoute"]
 
+myApp :: AppStateT ()
+myApp = do
+  addRoute routeHandler1 (== "handler1")
+  addRoute routeHandler2 (== "handler2")
+  addRoute routeHandler3 (== "handler3")
 
+main :: IO ()
+main = myScotty myApp
+
+-- framework functions
+
+addRoute' :: Route -> AppState -> AppState
+addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
+
+route :: (String -> String) -> (String -> Bool)
+  -> Route
 route mw pat mw1 input_string =
   let tryNext = mw1 input_string in
   if pat input_string
@@ -41,26 +58,29 @@ route mw pat mw1 input_string =
   else
     tryNext
 
+addRoute ::
+  Monad m =>
+  (String -> String) -> (String -> Bool) -> ST.StateT AppState m ()
 addRoute mf pat = ST.modify $ \s -> addRoute' (route mf pat) s
 
-myApp :: AppStateT ()
-myApp = do
-  addRoute routeHandler1 (== "handler1")
-  addRoute routeHandler2 (== "handler2")
-  addRoute routeHandler3 (== "handler3")
 
-runMyApp def my_app request = do
-  let s = ST.execState my_app AppState{ routes = []}
-  let output = foldl (flip ($)) def (routes s) request
+runMyApp def app_state request = do
+  let output = foldl (flip ($)) def (routes app_state) request
   return output
 
-main = do
+
+userInputLoop app_state = do
   putStrLn "Please type in the request"
   putStrLn "(one of 'handler1', 'handler2', 'handler3', 'buggy' or any string for default handling)"
   request <- getLine
   unless (request == "q") $ do
-    let response = runMyApp defaultRoute myApp request
+    let response = runMyApp defaultRoute app_state request
     case response of
       Just x -> putStrLn x
       Nothing -> putStrLn "Error"
-    main
+    userInputLoop app_state
+
+
+myScotty my_app = do
+    let app_state = ST.execState my_app AppState{ routes = []}
+    userInputLoop app_state
